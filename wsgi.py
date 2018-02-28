8ee6740010c38e81d3b898f0d1e26b5725ad9f8c
João Paulo Canário <jopacanario@gmail.com>
2018-02-28 14:56:55 -0300

    Add swagger to root endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
+# Load celery
 from leaderboard.extensions import celery
-from leaderboard import create_app, init_celery
+from leaderboard import create_app
+
+from flasgger import Swagger
 
 # Create App
-app = create_app()
-
-# Init celery
-init_celery(app)
+app = create_app(in_celery=True, in_swagger=True)
+Swagger(app)
